0db0d6abfcecf8ffd989c618c900983d8ae2bc35
'hi.java2' <'hi.java2@gmail.com'>
2020-12-03 09:45:17 +0900

    ShapeApp Triangle 추가

--- src/com/javaex/ex20/ShapeApp.java
+++ src/com/javaex/ex20/ShapeApp.java
@@ -1,11 +1,13 @@
 package com.javaex.ex20;
 
+import com.javaex.ex21.Triangle;
+
 public class ShapeApp {
 
 	public static void main(String[] args) {
 		
 		//Shape 배열만들기
-		Shape[] sArray = new Shape[2];
+		Shape[] sArray = new Shape[3];
 		
 		//사각형 만들기
 		//Ractangle r = new Ractangle("빨강", "빨강", 5, 5);
@@ -15,9 +17,15 @@ public class ShapeApp {
 		//Circle c = new Circle("파랑", "파랑", 5);
 		Shape c01 = new Circle("초록", "초록", 500);
 		
+		//삼각형 만들기
+		//Triangle c = new Triangle("보라", "보라", 10, 10);
+		Shape t01 = new Triangle("분홍", "분홍", 10, 10);
+				        
+		
 		//배열에 주소값 대입
 		sArray[0] = r01;
 		sArray[1] = c01;
+		sArray[2] = t01;
 		
 		//전부 그리기
 		for(int i=0; i<sArray.length; i++) {
--- src/com/javaex/ex21/Triangle.java
+++ src/com/javaex/ex21/Triangle.java
@@ -1,5 +1,54 @@
 package com.javaex.ex21;
 
-public class Triangle {
+import com.javaex.ex20.Shape;
+
+public class Triangle extends Shape {
+	// 필드
+	private int width;
+	private int height;
+
+	// 생성자
+	public Triangle() {
+	}
+
+	public Triangle(String fillColor, String lineColor, int width, int height) {
+		super(fillColor, lineColor);
+		this.width = width;
+		this.height = height;
+	}
+
+	// 메소드 g/s
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	// 메소드 - 일반
+	public void draw() {
+		System.out
+				.println("삼각형-면색:" + super.fillColor + "  선색:" + super.lineColor + "  가로:" + width + "  세로:" + height);
+	}
+
+	public double area() {
+		// double area = width * height / 2;
+		return width * height / 2;
+	}
+
+	@Override
+	public String toString() {
+		return "Triangle [width=" + width + ", height=" + height + ", fillColor=" + fillColor + ", lineColor="
+				+ lineColor + "]";
+	}
 
 }
